3c838a4ea421f615758edd65aaff2e84868a2a52
Lauren Hetherington <hetherington@astro.utoronto.ca>
2018-07-31 18:29:21 -0400

    Starting new loading system using xml

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -3,64 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
-    
-    public Texture2D LevelToLoad;
 
-    public ColorPrefab[] colorprefabs;
-    public ColorPrefab[] monsters;
-    Color defaultbg;
+    public TextAsset LevelToLoad;
+    public ObjLegend[] ObjectLegend;
+    ObjLegend[] SymbolLegend;
 	// Use this for initialization
 	void Awake () {
-        defaultbg = new Color(0,0,0,0);
+        //defaultbg = new Color(0,0,0,0);
         GenerateMap();
 	}
 
     void GenerateMap() {
 
-        Color pixelcolor;
-        int k;
-        for (int i = 0; i < LevelToLoad.width;i++) {
-            for (int j = 0; j < LevelToLoad.height;j++) {
-                k = 0;
-                pixelcolor = LevelToLoad.GetPixel(i, j);
-                if (Mathf.Approximately(pixelcolor.a,0f)) { continue; }
-                if (pixelcolor.a < 0.8f) { k = 1; }
-                Debug.Log(pixelcolor);
-                PixelToObject(pixelcolor, i, j, k);
 
-            }
-        }
     }
 
-    void PixelToObject(Color pixelcolor,int i, int j, int k) {
-        foreach (ColorPrefab colorprefab in colorprefabs)
+    void PixelToObject(string Symbol,int i, int j, int k) {
+        foreach (ObjLegend symLegend in SymbolLegend)
         {
             //Debug.Log(colorprefab.color);
-            if (Mathf.Approximately(pixelcolor.r, colorprefab.color.r) &&
-                Mathf.Approximately(pixelcolor.g, colorprefab.color.g) &&
-                Mathf.Approximately(pixelcolor.b, colorprefab.color.b))
+            if (Symbol.Contains(symLegend.ObjectName))
             {
-                Instantiate(colorprefab.prefab, new Vector3(i, j, k),
+                Instantiate(symLegend.prefab, new Vector3(i, j, k),
                             Quaternion.identity, transform);
-                if (k > 0 && Mathf.Approximately(defaultbg.a, 0f))
-                {
-                    defaultbg = colorprefab.color;
-                    defaultbg.a = 0.5f;
-                }
-                if (colorprefab.isObject && !Mathf.Approximately(defaultbg.a,0f))
-                {
-                    PixelToObject(defaultbg, i, j, 1);
-                    //Instantiate(defaultbg, new Vector3(i, j, 1), Quaternion.identity, transform);
-                }
             }
         }
     }
 
+    /*
     [System.Serializable]
     public class ColorPrefab {
         public Color color;
         public GameObject prefab;
         public bool isObject;
+    }*/
+
+    [System.Serializable]
+    public class ObjLegend {
+        public string ObjectName;
+        public GameObject prefab;
     }
 	
 }
