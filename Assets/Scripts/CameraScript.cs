fe25e49cb8dc4ec9c541301f9661d4ba71ec5571
Lauren Hetherington <hetherington@astro.utoronto.ca>
2018-08-01 12:48:25 -0400

    Basic XML based map system ready. Also, updated camera system to have contraints based on map size. Needs tweaking but works.

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+    GameObject player;
+    Rigidbody playerrb;
+    public Vector3 offset;
+    Vector3 defaultoffset;
+    public float t;
+    public GameObject LevGen;
+    float MinX, MaxX, MinZ, MaxZ;
+    Vector2 EdgeDist;
+    Vector3 targpos;
+    Camera cam;
+    //LevelGenerator levgenscript;
+    // Use this for initialization
+    void Start()
+    {
+        defaultoffset = offset + Vector3.zero;
+        player = GameObject.FindGameObjectWithTag("Player");
+        transform.position = player.transform.position + offset;
+        targpos = transform.position;
+        playerrb = player.GetComponent<Rigidbody>();
+        //levgenscript = LevGen.GetComponent<LevelGenerator>();
+        CalculateEdges();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        targpos = playerrb.position + offset;
+        if (targpos[0] < MinX) { targpos[0] = MinX; }
+        if (targpos[0] > MaxX) { targpos[0] = MaxX; }
+        if (targpos[1] < MinZ) { targpos[1] = MinZ; }
+        if (targpos[1] > MaxZ) { targpos[1] = MaxZ; }
+        transform.position = Vector3.Lerp(transform.position, targpos,t);
+	}
+
+    void CalculateEdges() {
+        cam = GetComponent<Camera>();
+        float baseedge = Mathf.Abs(offset[2] * Mathf.Tan(2 * Mathf.PI * cam.fieldOfView / 720));
+        EdgeDist = new Vector2(baseedge * cam.aspect, baseedge);
+        Debug.Log(EdgeDist);
+    }
+
+    public void SetBoundaries(int minx, int maxx, int minz, int maxz) {
+        Debug.Log("minx:"+minx);
+        Debug.Log("minz:"+minz);
+        Debug.Log("maxx:"+maxx);
+        Debug.Log("maxz:"+maxz);
+
+        bool movecloser = false;
+        int breaker = 0;
+        do
+        {
+            if (movecloser) {
+                offset[2] += 0.1f;
+                movecloser = false;
+            }
+            CalculateEdges();
+
+            MinX = minx + EdgeDist[0] - offset[0];
+            MaxX = maxx - EdgeDist[0] - offset[0];
+
+            if (MaxX < MinX)
+            {
+                Debug.Log("x=x");
+                MaxX = (MaxX + MinX) / 2f;
+                MinX = MaxX + 0f;
+                movecloser = true;
+            }
+
+            MinZ = minz + EdgeDist[1] - offset[1];
+            MaxZ = maxz - EdgeDist[1] - offset[1];
+
+            if (MaxZ < MinZ)
+            {
+                Debug.Log("z=z");
+                MaxZ = (MaxZ + MinZ) / 2f;
+                MinZ = MaxZ + 0f;
+                movecloser = true;
+            }
+
+            Debug.Log("MinX:" + MinX);
+            Debug.Log("MinZ:" + MinZ);
+            Debug.Log("MaxX:" + MaxX);
+            Debug.Log("MaxZ:" + MaxZ);
+            breaker++;
+        } while (movecloser && breaker<100);
+
+    }
+}
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,33 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
 
     public TextAsset LevelToLoad;
     public ObjLegend[] ObjectLegend;
-    ObjLegend[] SymbolLegend;
+    List<ObjLegend> SymbolLegend;
+    public Camera cam;
+    CameraScript cameraScript;
 	// Use this for initialization
 	void Awake () {
         //defaultbg = new Color(0,0,0,0);
-        GenerateMap();
+        SymbolLegend = new List<ObjLegend>();
+        cameraScript = cam.GetComponent<CameraScript>();
+        GenerateMap(LevelToLoad);
 	}
 
-    void GenerateMap() {
+    void GenerateMap(TextAsset dungeon, string mapid="screen0") {
+        XmlDocument dunxml = new XmlDocument();
+        dunxml.LoadXml(dungeon.text);
+        XmlNodeList legendnodes = dunxml.GetElementsByTagName("tile");
+        //SymbolLegend = new ObjLegend[legendnodes.Count];
+        SymbolLegend.Clear();
+        foreach (XmlNode tiletype in legendnodes) {
+            ObjLegend newtile = new ObjLegend();
+            foreach (XmlNode tileelement in tiletype.ChildNodes) {
+                if (tileelement.Name == "symbol") {
+                    newtile.ObjectName = tileelement.InnerText;
+                    //Debug.Log(tileelement.InnerText);
+                }
+                else if (tileelement.Name=="isobject") {
+                    newtile.prefab = SymbolToObject(tileelement.InnerText, ObjectLegend);
+                    //Debug.Log(newtile.prefab);
+                    //Debug.Log(tileelement.InnerText);
+                }
+            }
+            SymbolLegend.Add(newtile);
+        }
 
+        XmlNodeList mapnodes = dunxml.GetElementsByTagName("screen");
+        int k;
+        int minx = -100;
+        int maxx = 100;
+        int minz = -100;
+        int maxz = 100;
+        //XmlNode mapnode = dunxml.GetElementById("screen0");
+        foreach (XmlNode map in mapnodes) {
+            if (map.Attributes["id"].Value == mapid) {
+                //Debug.Log(map.InnerXml);
+                foreach (XmlNode mapdetail in map.ChildNodes) {
+                    if (mapdetail.Name=="grid") {
+                        k = int.Parse(mapdetail.Attributes["zpos"].Value);
+                        string[] grid=((mapdetail.InnerText.Replace(" ","")).Trim()).Split('\n');
 
+                        minz = -grid.Length+1;
+                        maxz = 0;
+                        minx = 0;
+                        maxx = grid[0].Length;
+
+                        for (int i = 0; i < grid.Length;i++) {
+                            Debug.Log(grid[i]);
+                            for (int j = 0; j < grid[i].Length;j++) {
+                                if (grid[i].Length-1 > maxx) { maxx = grid[i].Length-1; }
+
+                                GameObject nextobj = SymbolToObject(grid[i][j].ToString(), SymbolLegend.ToArray());
+                                if (nextobj != null) {
+                                    Instantiate(nextobj, new Vector3(j,-i,k),Quaternion.identity,transform);
+                                }
+                            }
+                        }
+                    }
+                }
+                cameraScript.SetBoundaries(minx, maxx, minz, maxz);
+
+            }
+
+        }
+        //Debug.Log(mapnode.InnerXml);
     }
 
-    void PixelToObject(string Symbol,int i, int j, int k) {
-        foreach (ObjLegend symLegend in SymbolLegend)
+    GameObject SymbolToObject(string Symbol,ObjLegend[] legends) {
+        GameObject toreturn = null;
+        foreach (ObjLegend symLegend in legends)
         {
             //Debug.Log(colorprefab.color);
             if (Symbol.Contains(symLegend.ObjectName))
             {
-                Instantiate(symLegend.prefab, new Vector3(i, j, k),
-                            Quaternion.identity, transform);
+                //Instantiate(symLegend.prefab, new Vector3(i, j, k),
+                //            Quaternion.identity, transform);
+                toreturn=symLegend.prefab;
             }
         }
+        return toreturn;
     }
 
     /*
